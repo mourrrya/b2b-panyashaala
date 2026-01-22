589c27a7ef750bbcd220ed3ba24ed68655efd0ea
mourrrya <mourrrya@gmail.com>
2026-01-23 02:44:14 +0530

    updated ignore

--- prisma/generated/prisma/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Re-export Prisma generated types for use throughout the codebase.
- * This file acts as a compatibility layer to replace @prisma/client imports.
- */
-
-// Export everything from the client module (includes PrismaClient, models, enums, Prisma namespace)
-export * from "./client.ts";
