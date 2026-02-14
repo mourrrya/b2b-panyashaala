9f8c083f03a9bd5392e57f3147b8a6efca21aae8
mourrrya <mourrrya@gmail.com>
2026-02-15 02:37:11 +0530

    refactor: Remove ProductDescription component and update exports

--- components/products/components/index.ts
+++ components/products/components/index.ts
@@ -1,4 +1,3 @@
-export { ProductDescription } from "./ProductDescription";
 export { ProductHeader } from "./ProductHeader";
 export { ProductSidebar } from "./ProductSidebar";
 export { ProductSpecifications } from "./ProductSpecifications";
